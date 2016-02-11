64d459f3e420159e7679aa9d27a4e244ac885965
Kay Cheng <puiki.cheng@gmail.com>
2016-02-11 07:23:13 +0000

    Update front-end styling

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,7 @@
 $(function() {
+  var myFunction = function(arg) {
+    console.log(arg);
+  }
 
   var mainCity;
 
@@ -17,21 +20,28 @@ $(function() {
       dataType: 'jsonp',
       jsonp: 'cb',
       success: function(cities) {
-        var resultList = $('<ul>').appendTo($('#city-results fieldset'));
+        var resultList = $('<div>').addClass('list-group').appendTo($('#city-results fieldset'));
         cities.RESULTS.forEach(function(city) {
-          $('<li>').text(city.name).attr('data-zmw', city.zmw).appendTo(resultList);
-          $('#city-results').show();
+          console.log(city);
+          console.log(city.zmw);
+          $('<button>').addClass('list-group-item').text(city.name).attr('data-zmw', city.zmw).appendTo(resultList);
+          $('#city-results').fadeIn('slow');
         });
       }
     });
 
-    $('#city-results').on('click', 'li', function() {
+    $('#city-results').on('click', 'button', function(e) {
       var cityID = $(this).data('zmw');
       var cityName = $(this).text();
-      var showURL = "http://api.wunderground.com/api/8b059408a6bc8652/forecast/q/zmw:" + cityID + ".json"
+      var showURL = "http://api.wunderground.com/api/8b059408a6bc8652/forecast/q/zmw:" + cityID + ".json";
       $.ajax({
         url: showURL,
+        dataType: 'jsonp',
         success: function(city) {
+          if(city.response.error) {
+            swal('Sorry. Could not find this data.');
+            return;
+          }
           var forecastToday = city.forecast.txt_forecast.forecastday[0];
           var forecastTmr = city.forecast.txt_forecast.forecastday[1];
           swal({
