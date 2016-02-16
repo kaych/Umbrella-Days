079c4a1796153c6dc66f9893fba3930ab5a1caa4
Kay Cheng <puiki.cheng@gmail.com>
2016-02-16 07:42:21 +0000

    Add new js code for new button and new swal styling

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,12 +1,34 @@
 $(function() {
-  var myFunction = function(arg) {
-    console.log(arg);
-  }
+  // var myFunction = function(arg) {
+  //   console.log(arg);
+  // }
 
-  var mainCity;
+  // var mainCity;
 
   $('#btn-start').on('click', function() {
     $('#city-form').slideDown();
+  });
+
+  $('#btn-geo-location').on('click', function() {
+    var geoURL = "http://api.wunderground.com/api/8b059408a6bc8652/forecast/q/autoip.json";
+    $.ajax({
+      url: geoURL,
+      dataType: 'jsonp',
+      success: function(city) {
+        if(city.response.error) {
+          swal('Sorry. Could not find this data.');
+          return;
+        }
+        var forecastToday = city.forecast.txt_forecast.forecastday[0];
+        var forecastOther = city.forecast.txt_forecast.forecastday;
+        swal({
+          title: "Local forecast (@ " + city.forecast.txt_forecast.date + ")",
+          imageUrl: forecastToday.icon_url,
+          width: 600,
+          html: "<em>" + forecastToday.fcttext_metric + "</em><br><br>" + "<table><tr><th>" + forecastOther[1].title + ": </th> <td>" + forecastOther[1].fcttext_metric + "</td></tr>" + "<tr><th>" + forecastOther[2].title + ": </th><td>" + forecastOther[2].fcttext_metric + "</td></tr>" + "<tr><th>" + forecastOther[4].title + ": </th><td>" + forecastOther[4].fcttext_metric + "</td> <tr><th>" + forecastOther[6].title + ": </th><td>" + forecastOther[6].fcttext_metric + "</td></tr></table>"
+        });
+      }
+    });
   });
 
   $('#searchCity').on('submit', function(e) {
@@ -47,7 +69,7 @@ $(function() {
           swal({
             title: "Current condition of " + cityName + " @ " + city.forecast.txt_forecast.date,
             imageUrl: forecastToday.icon_url,
-            text: "Forecast: " + forecastToday.fcttext_metric + "; Tomorrow: " + forecastTmr.fcttext_metric
+            html: "<b>Current Forecast: </b>" + forecastToday.fcttext_metric + "<br> <b> Tomorrow: </b>" + forecastTmr.fcttext_metric
           });
         }
       });
